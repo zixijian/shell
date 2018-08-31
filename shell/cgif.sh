7c17445a2146ae55f2589e5c58e671d2d1b1b384
zixijian <zixijian@gmail.com>
2018-08-31 23:52:53 +0800

    change cgif.sh

--- shell/cgif.sh
+++ shell/cgif.sh
@@ -1,11 +1,12 @@
 #written by zixijian
-#convert videos to HD gif 
+#convert videos to HD gif
+#example: bash cgif.sh 1.mp4 2.gif
 #!/bin/sh
 ffmpeg -v warning -i $1 -vf palettegen=max_colors=256 -y tmp.png
 
 palette="tmp.png"
 
-filters="fps=15,scale=480:-1:flags=lanczos"
+filters="fps=15,scale=280:-1:flags=lanczos"
 
 ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
 ffmpeg -v warning -i $1 -i $palette -b:v 1k -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
