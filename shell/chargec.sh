e22e9f0878daad73f0bdc5fee958c89f89480dff
zixijian <zixijian@gmail.com>
2021-04-15 11:49:57 +0800

    up

---
+++ shell/chargec.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Android chroot charge limit controller
+# author: zixijian
+# website: zixijian.github.io
+# co-author: Ethereal
+# useage: crontab -e
+#         */5 * * * * /usr/chargec.sh
+
+# define
+charge=`cat /sys/class/power_supply/battery/status`
+capacity=`cat /sys/class/power_supply/battery/capacity`
+
+echo Now battery is: $charge.
+echo Now battery level is: $capacity.
+
+# set max charge current 
+# parameter: 800mA=800000
+chmod 644 /sys/class/power_supply/battery/constant_charge_current_max
+echo 800000 > /sys/class/power_supply/battery/constant_charge_current_max
+
+
+# check max limit
+if [ $capacity -ge 85 ]; then
+# stop
+  echo 1 > /sys/class/power_supply/battery/input_suspend 
+fi
+
+# check min limit
+if [ $capacity -le 35 ]; then
+# start 
+  echo 0 > /sys/class/power_supply/battery/input_suspend
+fi
