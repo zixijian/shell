69d2880aaf24412b4deb10aacf4fd3875ee83ae8
zixijian <zixijian@gmail.com>
2021-04-18 09:50:37 +0800

    up

--- shell/chargec.sh
+++ shell/chargec.sh
@@ -18,7 +18,7 @@ echo Now battery level is: $capacity.
 chmod 644 /sys/class/power_supply/battery/constant_charge_current_max
 echo 800000 > /sys/class/power_supply/battery/constant_charge_current_max
 
-
+# Redminote7pro violet,Redminote9pro gauguin 
 # check max limit
 if [ $capacity -ge 85 ]; then
 # stop
@@ -30,3 +30,18 @@ if [ $capacity -le 35 ]; then
 # start 
   echo 0 > /sys/class/power_supply/battery/input_suspend
 fi
+
+:<<!
+# Redminote4x mido 
+# check max limit
+if [ $capacity -ge 85 ]; then
+# stop
+  echo 0 > /sys/class/power_supply/battery/charging_enabled
+fi
+
+# check min limit
+if [ $capacity -le 35 ]; then
+# stop
+  echo 1 > /sys/class/power_supply/battery/charging_enabled
+fi
+!
