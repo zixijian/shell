acd311d2785feddbf2f292b7c339a2f82f6a05d8
zixijian <zixijian@gmail.com>
2022-10-07 00:52:12 +0800

    fix pkg com

--- shell/tt4t.sh
+++ shell/tt4t.sh
@@ -33,7 +33,7 @@ echo "-->替换软件源为国内清华开源镜像站..."
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
 sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
 sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
-pkg upgrade -y
+pkg update && pkg upgrade -y
 
 #安装tintin相关软件
 echo "-->安装 tintin++ 及相关软件..."
