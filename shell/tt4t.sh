5b14862645f9981e0a183a47e319faa6e849f580
zixijian <zixijian@gmail.com>
2022-10-07 11:18:30 +0800

    fix sources

--- shell/tt4t.sh
+++ shell/tt4t.sh
@@ -30,9 +30,7 @@ ln -s ~/storage/shared/tintin ~/tt
 
 #替换清华源
 echo "-->替换软件源为国内清华开源镜像站..."
-sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
-sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
-sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list
 pkg update && pkg upgrade -y
 
 #安装tintin相关软件
